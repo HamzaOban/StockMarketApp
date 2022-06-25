5428b99a35a402b46a61aa04a45d4d37209233d5
Hamza Oban <hamzaoban3@gmail.com>
2022-06-25 16:37:54 +0300

    CompanyListingViewModel

--- app/src/main/java/com/dogukan/stockmarketapp/domain/repository/StockRepository.kt
+++ app/src/main/java/com/dogukan/stockmarketapp/domain/repository/StockRepository.kt
@@ -1,6 +1,7 @@
 package com.dogukan.stockmarketapp.domain.repository
 
 import com.dogukan.stockmarketapp.data.local.CompanyListingEntity
+import com.dogukan.stockmarketapp.domain.model.CompanyListing
 import com.dogukan.stockmarketapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 
@@ -9,5 +10,5 @@ interface StockRepository {
     suspend fun getCompanyListing(
         fetchFromRemote : Boolean,
         query : String
-    ) : Flow<Resource<List<CompanyListingEntity>>>
+    ) : Flow<Resource<List<CompanyListing>>>
 }
